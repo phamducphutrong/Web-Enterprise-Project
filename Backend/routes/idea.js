29d818ffb9d3072347534fc00420c4ac3bef8d2e
ManhTung64 <94000462+ManhTung64@users.noreply.github.com>
2023-02-14 02:29:52 +0700

    update category & idea

--- Backend/routes/idea.js
+++ Backend/routes/idea.js
@@ -5,128 +5,123 @@ const Idea = require('../models/Idea')
 const User = require('../models/User')
 const router = require('./auth')
 
-//@route POST api/ideas
-//@desc Create post
-//@access Private
-// router.post('/', verifyToken, async(req, res) => {
-//     const{ Title, Description, LastEdition } = req.body
+// @route POST api/ideas
+// @desc Create idea
+// @access Private
+router.post('/', verifyToken, async(req, res) => {
+    const{ Title, Description, LastEdition } = req.body
 
-//     if(!Title)
-//     return res.status(400).json({success: false, message: 'Title is required!'})
+    if(!Title)
+    return res.status(400).json({success: false, message: 'Title is required!'})
 
-//     try {
-//         const newIdea = new Idea({
-//             Title, 
-//             Description, 
-//             LastEdition, 
-//             UserId: req.UserId,
-//             CategoryId: '63e91195e2f49701e0175481'
-//         })
+    try {
+        const newIdea = new Idea({
+            Title, 
+            Description, 
+            LastEdition, 
+            UserId: req.UserId,
+            CategoryId: '63e91195e2f49701e0175481'
+        })
 
-//         await newIdea.save()
+        await newIdea.save()
 
-//         res.json({success: true, message: 'Successfully', post: newIdea})
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({success: false, message:'Internla server error'})
-//     }
-// })
+        res.json({success: true, message: 'Successfully', post: newIdea})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, message:'Internla server error'})
+    }
+})
 
-// // @route GET api/posts
-// // @desc Get posts
-// // @access Private
-// router.get('/', verifyToken, async (req, res) => {
-// 	try {
-// 		const ideas = await Idea.find({ user: req.UserId }).populate('user', [
-// 			'Name'
-// 		])
-// 		res.json({ success: true, ideas })
-// 	} catch (error) {
-// 		console.log(error)
-// 		res.status(500).json({ success: false, message: 'Internal server error' })
-// 	}
-// })
+// @route GET api/idea
+// @desc Get owner ideas
+// @access Private
+router.get('/', async (req, res) => {
+	try {
+		const ideas = await Idea.find({ user: req.UserId }).populate('UserId', 'Name')
+		res.json({ success: true, ideas })
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
+})
 
-// // @route PUT api/posts
-// // @desc Update post
-// // @access Private
-// router.post('/:id', verifyToken, async(req, res) => {
-//     const{ Title, Description, LastEdition } = req.body
+// @route PUT api/idea
+// @desc Update idea
+// @access Private
+router.post('/:id', verifyToken, async(req, res) => {
+    const{ Title, Description, LastEdition } = req.body
 
-//     if(!Title)
-//     return res.status(400).json({success: false, message: 'Title is required!'})
+    if(!Title)
+    return res.status(400).json({success: false, message: 'Title is required!'})
 
-//     try {
-//         let updatedPost = {
-//             Title,
-//             Description,
-//             LastEdition,
-//         }
+    try {
+        let updatedPost = {
+            Title,
+            Description,
+            LastEdition,
+        }
 
-//         const postUpdateCondition = { _id: req.params.id, user: req.UserId }
+        const postUpdateCondition = { _id: req.params.id, user: req.UserId }
 
-// 		updatedPost = await Post.findOneAndUpdate(
-// 			postUpdateCondition,
-// 			updatedPost,
-// 			{ new: true }
-// 		)
+		updatedPost = await Post.findOneAndUpdate(
+			postUpdateCondition,
+			updatedPost,
+			{ new: true }
+		)
 
-//         // User not authorised to update post or post not found
-// 		if (!updatedPost)
-//         return res.status(401).json({
-//             success: false,
-//             message: 'Post not found or user not authorised'
-//         })
+        // User not authorised to update post or post not found
+		if (!updatedPost)
+        return res.status(401).json({
+            success: false,
+            message: 'Post not found or user not authorised'
+        })
 
-//     res.json({
-//         success: true,
-//         message: 'Excellent progress!',
-//         post: updatedPost
-//     })
+    res.json({
+        success: true,
+        message: 'Excellent progress!',
+        post: updatedPost
+    })
 
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({success: false, message:'Internla server error'})
-//     }
-// })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, message:'Internla server error'})
+    }
+})
 
-// // @route DELETE api/posts
-// // @desc Delete post
-// // @access Private
-// router.delete('/:id', verifyToken, async (req, res) => {
-// 	try {
-// 		const postDeleteCondition = { _id: req.params.id, user: req.UserId }
-// 		const deletedPost = await Post.findOneAndDelete(postDeleteCondition)
+// @route DELETE api/idea
+// @desc Delete idea
+// @access Private
+router.delete('/:id', verifyToken, async (req, res) => {
+	try {
+		const postDeleteCondition = { _id: req.params.id, user: req.UserId }
+		const deletedPost = await Post.findOneAndDelete(postDeleteCondition)
 
-// 		// User not authorised or post not found
-// 		if (!deletedPost)
-// 			return res.status(401).json({
-// 				success: false,
-// 				message: 'Post not found or user not authorised'
-// 			})
+		// User not authorised or post not found
+		if (!deletedPost)
+			return res.status(401).json({
+				success: false,
+				message: 'Post not found or user not authorised'
+			})
 
-// 		res.json({ success: true, post: deletedPost })
-// 	} catch (error) {
-// 		console.log(error)
-// 		res.status(500).json({ success: false, message: 'Internal server error' })
-// 	}
-// })
+		res.json({ success: true, post: deletedPost })
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
+})
 
-// // @route GET api/posts
-// // @desc Get posts
-// // @access Private
-// router.get('/home', verifyToken, async (req, res) => {
-// 	try {
-        
-// 		const ideas = await Idea.find().sort({createdAt: -1}).populate('Category', [
-// 			'Title'
-// 		])
-// 		res.json({ success: true, ideas })
-// 	} catch (error) {
-// 		console.log(error)
-// 		res.status(500).json({ success: false, message: 'Internal server error' })
-// 	}
-// })
+// @route GET api/idea/home
+// @desc Get all ideas
+// @access Private
+router.get('/home', verifyToken, async (req, res) => {
+	try {
+        const ideas = await Idea.find().sort({creatAt: -1}).populate('UserId','Name')
+		res.json({ success: true, ideas})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ success: false, message: 'Internal server error' })
+	}
+})
 
 module.exports = router
 
