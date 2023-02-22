b0fd279adc8c54b534e3410368757768aa867049
ManhTung64 <94000462+ManhTung64@users.noreply.github.com>
2023-02-22 11:58:17 +0700

    update

--- Frontend/web-enterprise/src/App.js
+++ Frontend/web-enterprise/src/App.js
@@ -3,6 +3,7 @@ import LoginForm from './components/auth/LoginForm';
 import Homepage from './components/homepage/Homepage';
 import Category from './components/category/Category';
 import Test from "./components/idea/test";
+import Profile from './components/profile/Profile'
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/category" element={<Category />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
